dd64591b3d7b0bda4a1d8858979a782794a3031a
IngridAkeida <ingridsanchesp@gmail.com>
2025-11-25 15:06:56 +0100

    feat: add filter and search sources

--- script.js
+++ script.js
@@ -1,42 +1,65 @@
 let cardContainer = document.querySelector(".card-container");
+let searchField = document.querySelector("header input");
 let datas = [];
 
 async function startSearch(){
-  let response = await fetch("data.json");
-  datas = await response.json();
-  renderCards(datas);
-  console.log(datas);
+  if( datas.length === 0){
+    try{
+      let response = await fetch("data.json");
+      datas = await response.json();
+    } catch {
+      console.error("failed to fetch", error);
+      return;
+    }
+  }
+  
+  const searchTerm = searchField.value.toLowerCase();
+  let filteredData = datas.filter(data => {
+      
+    const nameMatch = data.name.toLowerCase().includes(searchTerm);
+      
+    const ingredientMatch = data.ingredients.some(ing => ing.item.toLowerCase().includes(searchTerm));
+    
+    return nameMatch || ingredientMatch;
+  });
+
+    renderCards(filteredData);
+
 }
 
 function renderCards(datas){
+  cardContainer.innerHTML = "";
   for (let data of datas) {
     let article = document.createElement("article");
     article.classList.add("recipe-card");
+    
+    let ingredientsList = data.ingredients
+    .map(ing => `
+      <li class="ingredient-item">
+        <span class="item-quantity">${ing.quantity}</span> ${ing.item}
+      </li>`)
+    .join("");
+    
     article.innerHTML = `
       <div class="recipe-image-container">
         <img src="https://cdn.europosters.eu/image/750/34557.jpg" alt="A imagem de um espeto de carne grelhado, pronto para consumo." class="recipe-image">
       </div>
 
       <div class="recipe-info">
-          <h3 class="recipe-name">Meat Skewer</h3>
-          <p class="recipe-type">Type: Skewer</p>
-          <p class="recipe-description">An easy blend of meat and mushrooms grilled to restore health.</p>
-
+          <h3 class="recipe-name">${data.name}</h3>
+          <p class="recipe-type">Type: ${data.type}</p>
+          <p class="recipe-description">${data.description}</p>
           <div class="recipe-details">
               <p class="recipe-effects">
-                  **Effects:** <span class="effect-hearts">3 Hearts</span>
+                  <strong>Effects:</strong> <span class="effect-hearts">${data.effects.hearts} Hearts</span> 
+                  <strong>Uses:</strong> <span class="effect-uses">${data.uses}</span> 
               </p>
               
               <div class="recipe-ingredients">
                   <h4>Ingredients:</h4>
                   <ul role="list">
-                      <li class="ingredient-item">
-                          <span class="item-quantity">1x</span> Raw Meat
-                      </li>
-                      <li class="ingredient-item">
-                          <span class="item-quantity">1x</span> Any Mushroom
-                      </li>
-                  </ul>
+                    ${ingredientsList}
+                </ul>
               </div>
           </div>
       </div>
